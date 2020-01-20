0e7967d853a9785e134b04fd4f2d960f4ea32d13
stijnvanhoey <stijnvanhoey@gmail.com>
2020-01-20 19:54:55 +0100

    Update doc example pages

--- getting-started/_static/documentation_options.js
+++ getting-started/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.0rc0+116.g1e777eb25.dirty',
+    VERSION: '1.0.0rc0+118.g9534c755e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
